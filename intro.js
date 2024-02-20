db3e39b7e9c25531aed398191c732c913d31ade4
Charbel Akiki <charbelakiki18@gmail.com>
2024-02-20 14:44:33 +0200

    first commit

--- intro.js
+++ intro.js
@@ -1,11 +1,12 @@
 const http = require('http')
+
 const server = http.createServer((req,res)=>{
-    if(req.url === '/'){
-        res.end('hi')
-    }
-    if(req.url==='/about'){
-        res.end('bye')
-    }
+    console.log("Request")
+    res.end("Response")
 })
 
-server.listen(5000)
+server.listen(5000,()=>{
+    console.log("Server Listening")
+})
+
+console.log("Done")
